c9aef4791c9bff55016ac6bd289f9f04983306b7
astronomicalRedwoods27 <jacob.lyons1@aa.com>
2023-01-12 14:18:35 -0700

    fix: typo in release.config.js

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: "master",
+  branches: "main",
   repositoryUrl: "https://github.com/astronomicalRedwoods27/react-app/",
   plugins: [
     "@semantic-release/commit-analyzer",
