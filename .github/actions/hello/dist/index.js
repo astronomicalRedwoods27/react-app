cf1a8ad1ad8573e114baa014f4f8bcfbf4003529
astronomicalRedwoods27 <jacob.lyons1@aa.com>
2023-02-01 13:59:42 -0700

    refactor(main): added core action logging

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1278,14 +1278,23 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  throw( new Error("some error message"));
+  // throw( new Error("some error message"));
+  core.debug("Debug Message");
+  core.warning("Warning Message");
+  core.error("Error Message");
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name); // appears as ***
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging GitHub object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
